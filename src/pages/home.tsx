c896cc6c62511d65dc01f1900d8cbac66a94f2ef
Austin Montoya <austin.r.montoya@gmail.com>
2023-03-06 21:25:07 -0800

    fix imports

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,5 @@
-import styles from '../styles/home.module.css';
+import styles from "@/styles/home.module.css";
 
 export default function HomePage() {
-	return <div className={styles.homepage}>hello</div>
+  return <div className={styles.homepage}>hello</div>;
 }
