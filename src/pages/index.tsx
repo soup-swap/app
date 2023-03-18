836e6e2e2887741916185047344f7983731004c2
Austin Montoya <austin.r.montoya@gmail.com>
2023-03-17 17:16:59 -0700

    fix typescript errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,33 @@
-import { supabase } from './../lib/supabaseClient';
+import { supabase } from "./../lib/supabaseClient";
 
-function Page({ countries }) {
+interface Country {
+  id: string;
+  name: string;
+}
+
+interface ExamplePage {
+  countries: Country[];
+}
+
+function Page({ countries }: ExamplePage) {
   return (
-	<ul>
-	  {countries.map((country) => (
-		<li key={country.id}>{country.name}</li>
-	  ))}
-	</ul>
+    <ul>
+      {countries.map((country) => (
+        <li key={country.id}>{country.name}</li>
+      ))}
+    </ul>
   );
 }
 
 export async function getServerSideProps() {
-  let {data} = await supabase.from('countries').select()
-  console.log(data)
+  let { data } = await supabase.from("countries").select();
+  console.log(data);
 
   return {
-	props: {
-	 countries: data
-	},
-  }
+    props: {
+      countries: data,
+    },
+  };
 }
 
 export default Page;
